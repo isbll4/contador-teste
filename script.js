74cf836f1390946aa569f0ee7c332d835f3be885
Isabella Jacques e Gabriel <isabellajacquesegabriel@gmail.com>
2025-05-14 15:51:43 -0300

    add script update

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('oi')
+// Estado da Aplicação
+let numero = 0
+
+// variaveis da Aplicação
+
+function aumentar() {
+    numero++
+    update()
+
+}
+
+function anular() {
+    numero = 0
+    update()
+
+}
+
+function diminuir() {
+    numero--
+    update()
+
+}
+
+// Atualizar estado da aplicação
+ 
+function update() {
+    const p = document.querySelector('#resultado')
+    p.innerText = numero
+}
+
+// iniciar
+update()
